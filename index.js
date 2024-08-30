eff15b6d008b192486997ed7d18e5050c95ae30b
ravi9879 <ravigupta77389@gmail.com>
2024-08-30 11:26:49 +0530

    first

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ app.get('/' , (req,res) => {
     res.send("hi") ;
 })
 
+
+app.use('/', require('./router/add-cart'));
+app.use('/', require('./router/delete'));
+app.use('/', require('./router/login'));
+app.use('/', require('./router/sign_in'));
+app.use('/', require('./router/data'));
+app.use('/', require('./router/get-cart'));
+// app.use('/', require('./router/update'));
+
+
 app.listen(800 , () => {
     console.log("listening") ;
 })

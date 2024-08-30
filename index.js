75c499dfb99ee47e85fa9fce7e2c96cbc220c8e1
ravi9879 <ravigupta77389@gmail.com>
2024-08-30 11:11:42 +0530

    first

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express') ;
 const mconnect = require('./db')
+const express = require('express');
+const cp = require('cookie-parser');
+const cors = require('cors');
+const dy = require('body-parser');
+const app = express();
+
 
 mconnect() ;
 
-const app = express() ;
+
+app.use(cp());
+app.use(cors());
+app.use(dy.json());
+app.use(dy.urlencoded({ extended: true }));
+ 
 
 app.get('/' , (req,res) => {
     res.send("hi") ;

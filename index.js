bbb4970fcf8c487366b5362fc53ab47d3aff68c0
ravi9879 <ravigupta77389@gmail.com>
2024-08-30 11:44:05 +0530

    first

--- index.js
+++ index.js
@@ -16,17 +16,16 @@ mconnect() ;
  
 
 app.get('/' , (req,res) => {
-    res.send("hi6") ;
+    res.send("hi7") ;
 })
+ 
 
-
-// app.use('/', require('./router/add-cart'));
-// app.use('/', require('./router/delete'));
-// app.use('/', require('./router/login'));
-// app.use('/', require('./router/sign_in'));
-// app.use('/', require('./router/data'));
-// app.use('/', require('./router/get-cart'));
-// app.use('/', require('./router/update'));
+app.use('/', require('./router/add-cart'));
+app.use('/', require('./router/delete'));
+app.use('/', require('./router/login'));
+app.use('/', require('./router/sign_in'));
+app.use('/', require('./router/data'));
+app.use('/', require('./router/get-cart')); 
 
 
 app.listen(800 , () => {

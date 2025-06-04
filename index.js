0bb99994c72d5c798c91075f844466a05a1b7554
ravi9879 <ravigupta77389@gmail.com>
2025-06-04 19:21:34 +0530

    Resolve merge conflicts

--- index.js
+++ index.js
@@ -42,3 +42,58 @@ app.use('/', require('./router/check-auth'));
 app.listen(800 , () => {
     console.log("listening") ;
 })
+
+
+
+
+
+
+// const router = require('express').Router();
+// const { getSimilarProducts } = require('./ml'); 
+
+// const sampleProducts = [
+//   { id: 1, name: 'Red Nike Running Shoes' },
+//   { id: 2, name: 'Adidas Gym Shorts' },
+//   { id: 3, name: 'Apple iPhone 13 Pro' },
+//   { id: 4, name: 'Wireless Bluetooth Headphones' },
+// ];
+
+// router.post('/search', async (req, res) => {
+//   const { query } = req.body;
+//   const results = await getSimilarProducts(query, sampleProducts);
+//   res.json({ results });
+// }); 
+
+
+
+
+// const use = require('@tensorflow-models/universal-sentence-encoder');
+// const tf = require('@tensorflow/tfjs-node');
+
+// let model;
+
+// async function loadModel() {
+//   if (!model) {
+//     model = await use.load();
+//   }
+//   return model;
+// }
+
+// async function getSimilarProducts(query, products) {
+//   const model = await loadModel();
+//   const productNames = products.map(p => p.name);
+//   const embeddings = await model.embed([query, ...productNames]);
+
+//   const queryEmbedding = embeddings.slice([0, 0], [1]);
+//   const productEmbeddings = embeddings.slice([1, 0]);
+//   const scores = await tf.matMul(queryEmbedding, productEmbeddings, false, true).array();
+
+//   const ranked = products
+//     .map((p, i) => ({ ...p, score: scores[0][i] }))
+//     .sort((a, b) => b.score - a.score);
+
+//   return ranked.slice(0, 5); // top 5 results
+// }
+
+// module.exports = { getSimilarProducts };
+

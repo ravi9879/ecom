25080dfab5180885aa8d7058199a208e5e79ad55
ravi9879 <ravigupta77389@gmail.com>
2025-06-04 21:59:34 +0530

    ravi

--- db.js
+++ db.js
@@ -1,13 +1,8 @@
-const mongoose = require("mongoose") ;
-<<<<<<< HEAD
+const mongoose = require("mongoose") ; 
 // const mURL = "mongodb+srv://ravigupta77389:Ravi$500@cluster0.3nuxwpq.mongodb.net/";
 // const mURL = "mongodb+srv://ravigupta77389:Ravi500@ravip.d9vmqop.mongodb.net/?retryWrites=true&w=majority&appName=ravip" 
-const mURL = "mongodb://localhost:27017"  ;
-=======
-//const mURL = "mongodb+srv://ravigupta77389:Ravi$500@cluster0.3nuxwpq.mongodb.net/";
-const mURL = "mongodb+srv://ravigupta77389:Ravi500@ravip.d9vmqop.mongodb.net/?retryWrites=true&w=majority&appName=ravip" 
-// const mURL = "mongodb://localhost:27017"  ;
->>>>>>> 437fa808347cd6ec2fc54e31241195b1737a7922
+// const mURL = "mongodb://localhost:27017"  ; 
+const mURL = "mongodb+srv://ravigupta77389:Ravi500@ravip.d9vmqop.mongodb.net/?retryWrites=true&w=majority&appName=ravip"  
 const mconnect = () => {
     if(mongoose.connect(mURL)) { 
         console.log("connected");
--- router/check-auth.js
+++ router/check-auth.js
@@ -4,18 +4,15 @@ const jwt = require('jsonwebtoken');
 
 
 router.get('/check-auth', (req, res) => { 
-    try {
-        // const token = req.get('Cookie')?.split('=')[1] || false 
+    try { 
+        
         const token = req.cookies.token || false // Get token from cookies;
         // console.log("Token from cookies:", token);
         if (!token) {
             return res.send({ loggedIn: false});
-        }
-        // // Verify JWT token
-        const decoded = jwt.verify(token, "jwt-secret-key");
-        // Optionally, you can fetch user info here if needed
-        return res.send({ loggedIn: true, user: decoded.id });
-        // return res.json("ravi");
+        } 
+        const decoded = jwt.verify(token, "jwt-secret-key"); 
+        return res.send({ loggedIn: true, user: decoded.id }); 
     } catch (err) {
         return res.send({ loggedIn: false });
     }
